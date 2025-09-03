7a8cc4f7f93b0fbde2b8fa602c4dd6efdb724364
Deyue Li <lidy19@fudan.edu.cn>
2025-09-03 16:39:34 +0800

    code.py fixed

--- lab00/code.py
+++ lab00/code.py
@@ -7,9 +7,14 @@ def get_airspeed_velocity_of(unladen_swallow):
 # pretend there's code here...
 
 def fizzbuzz(num):
-  if num == 3: # edit this line
+  mul_of_3 = num % 3 == 0 # edit this line
+  mul_of_5 = num % 5 == 0 # edit this line
+  mul_of_5_and_3 = mul_of_3 and mul_of_5 # edit this line
+  if mul_of_5_and_3: # edit this line
+    print(f"{num}: fizzbuzz")
+  elif mul_of_3: # edit this line
     print(f"{num}: fizz")
-  if num == 5: # edit this line
+  if mul_of_5: # edit this line
     print(f"{num}: buzz")
 
 for i in range(1, 20):
